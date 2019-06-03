6268cdfd626d4cea87d5b1946f73c85800eb0544
basilcea <ogbonna.basil3@gmmail.com>
2019-06-03 14:39:32 +0100

    create post component - import font-awesome icon using npm

--- instagram-app/src/components/PostContainer/post.js
+++ instagram-app/src/components/PostContainer/post.js
@@ -1,2 +1,19 @@
 import React from 'react';
-import
+import './post.css';
+import { farHeart } from "@fortawesome/free-regular-svg-icons";
+import { farComment } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Post = (props) => {
+    return (
+        <div>
+         <img src={props.url} alt={''}/>
+        <div>
+        <FontAwesomeIcon icon={farHeart} />
+        <FontAwesomeIcon icon={farComment} />
+        </div>
+         <p>{props.likes}  likes</p>
+        </div>
+    )
+}
+export default Post
